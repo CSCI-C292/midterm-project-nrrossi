5ec43ecdeafdf73f4082f11e8b64421e43a8823d
Nicholas Rossi <nrrossi@iu.edu>
2020-10-20 21:33:29 -0400

    Assets and Player
    All assets are added including Dispenser, background, Truck and rock.
    
    PLayer has been added and placed at top of screen. Moves with mouse but is locked so it cannot go off screen.

---
+++ Fill er' Up/Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Assigns player the position of the mouse to control left and right movement
+        //while locking the Y axis
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePos.x, transform.position.y, 0);
+
+        //Prevents player from moving off screen
+        if(mousePos.x > 8.4){
+            transform.position = new Vector3(8.4f, transform.position.y, 0);
+        }
+        
+        //Prevents player from moving off screen
+        if(mousePos.x < -8.4){
+            transform.position = new Vector3(-8.4f, transform.position.y, 0);
+        }
+    }
+}
---
+++ Fill er' Up/Assets/Rock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    [SerializeField] float _rockFallSpeed = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(0, _rockFallSpeed, 0);
+    }
+}
