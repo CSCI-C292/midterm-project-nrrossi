d8c4e84b3f12be7df3fda1587aa7f844cb55530e
Nicholas Rossi <nrrossi@iu.edu>
2020-11-05 22:22:30 -0500

    Finished
    Couldnt get the truck to move like I wanted, Overall I am satisfied with what I accomplished. I'll dabble with it later. Enjoy!

--- Fill er' Up/Assets/DumpTruck.cs
+++ Fill er' Up/Assets/DumpTruck.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class DumpTruck : MonoBehaviour
 {
 
+
+    public static bool moveTruck = false;
+    private void Update() {
+
+        if(moveTruck)
+        //transform.position += new Vector3(0, Time.deltaTime * 2f, 0);
+        transform.position += new Vector3(Time.deltaTime * .5f, 0, 0);
+    }
+
+
+
  public static DumpTruck Instance;
 
     private void Awake() {
@@ -14,9 +25,27 @@ public class DumpTruck : MonoBehaviour
     public void MoveDumpTruck(){
 
     
-
+    //InvokeRepeating("Rumble", 0, 1f); //Rumble does not work...
+    
+    //moveTruck = true;
 
     }
 
+    void MoveOff(){
+    transform.position += new Vector3(Time.deltaTime * .1f, 0, 0);
 
+    }
+
+    void Rumble(){
+
+        float counter = 0f;
+
+     transform.position += new Vector3(0, 5f, 0);
+     while(counter < .5)
+         counter += Time.deltaTime;
+     
+     transform.position -= new Vector3(0, 5f, 0);
+    }
+
+    
 }
